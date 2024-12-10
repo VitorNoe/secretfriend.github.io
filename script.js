84bac3b96fece4079ce94fde5411842dcf7d8c88
VitorNoe <162385647+VitorNoe@users.noreply.github.com>
2024-12-10 20:42:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
+// Animação de neve
 const snowContainer = document.getElementById('snow-container');
 
 function createSnowflake() {
     const snowflake = document.createElement('div');
     snowflake.classList.add('snowflake');
-    snowflake.textContent = '•'; // Ponto branco
-    snowflake.style.left = `${Math.random() * 100}vw`; // Posição horizontal aleatória
-    snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`; // Duração aleatória (2 a 5 segundos)
-    snowflake.style.fontSize = `${Math.random() * 10 + 5}px`; // Tamanho aleatório (5 a 15px)
+    snowflake.textContent = '•';
+    snowflake.style.left = `${Math.random() * 100}vw`;
+    snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`;
+    snowflake.style.fontSize = `${Math.random() * 10 + 5}px`;
 
     snowContainer.appendChild(snowflake);
 
-    // Remove o floco após a animação
     setTimeout(() => {
         snowflake.remove();
-    }, 5000); // Tempo máximo da animação
+    }, 5000);
 }
 
-// Cria novos flocos de neve continuamente
 setInterval(createSnowflake, 200);
